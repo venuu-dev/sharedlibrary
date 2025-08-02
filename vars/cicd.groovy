f776cb979324814781b48de5fec00f66e8b1b8e5
venuu-dev <vdevops655@gmail.com>
2025-08-02 15:34:55 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,8 @@ def BuildArtifact()
 {
    sh 'mvn package'
 }  
+
+def Deployment(ip,app)
+{
+  sh "scp /var/lib/jenkins/workspace/Declarative01/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${app}.war"
+}  
